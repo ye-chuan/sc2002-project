7f1e72775359a6aab3f701f0ca235e2b0c4f2a82
ye-chuan <sengyechuan@gmail.com>
2023-11-26 06:22:39 +0800

    CampInformation now defaults to having empty list of faculties

--- src/scs3grp5/entity/CampInformation.java
+++ src/scs3grp5/entity/CampInformation.java
@@ -2,23 +2,36 @@ package scs3grp5.entity;
 import java.io.Serializable;
 import java.util.*;
 
-/** A "Dataclass" of Camp Information (should only be used internally / package)
+/**
+ * A "Dataclass" of Camp Information (should only be used internally / package)
+ * @author Lee Ye Chuan
+ * @version 1.0
+ * @since 2023-11-26
  */
 public class CampInformation implements Serializable {
 
-	private String campID;
-	private String name;
-	private String location;
-	private DateRange campDates;
-	private Date closingDate;
-	private int participantSlots;
-	private int campCommSlots;
-	private String description;
-	private Staff staffInCharge;
-	private Collection<Faculty> openTo;
+    /** The unique ID for this camp (for database purposes) */
+    private String campID;
+    /** Name of the camp */
+    private String name;
+    /** Location where the camp is held */
+    private String location;
+    /** Camp Dates as a range */
+    private DateRange campDates;
+    /** Registration's Last Date */
+    private Date closingDate;
+    /** Total slots for participants (not camp committee) */
+    private int participantSlots;
+    /** Total slots for camp committee members (not participants) */
+    private int campCommSlots;
+    /** Text description of the Camp */
+    private String description;
+    /** The staff in charge of the camp (cannot be changed) */
+    private Staff staffInCharge;
+    /** The collection of faculties that this camp is open to */
+    private Collection<Faculty> openTo = new ArrayList<Faculty>();
 
     /**
-     *
      * @param staffInCharge The only required (and unchangeable) attribute is the staff-in-charge
      */
     public CampInformation(Staff staffInCharge) {
@@ -29,20 +42,20 @@ public class CampInformation implements Serializable {
         this.campCommSlots = 0;
     }
 
-	/**
+    /**
      * Constructs the Camp Information object exhausively
-	 * 
-	 * @param name
-	 * @param location
-	 * @param startDate
-	 * @param endDate
-	 * @param closingDate
-	 * @param participantSlots
-	 * @param campCommSlots
-	 * @param staffInCharge
-	 * @param openTo
-	 */
-	CampInformation(String name, String location, Date startDate, Date endDate, Date closingDate, int participantSlots, int campCommSlots, Staff staffInCharge, Collection<Faculty> openTo) {
+     * 
+     * @param name
+     * @param location
+     * @param startDate
+     * @param endDate
+     * @param closingDate
+     * @param participantSlots
+     * @param campCommSlots
+     * @param staffInCharge
+     * @param openTo
+     */
+    CampInformation(String name, String location, Date startDate, Date endDate, Date closingDate, int participantSlots, int campCommSlots, Staff staffInCharge, Collection<Faculty> openTo) {
         this.name = name;
         this.location = location;
         this.closingDate = closingDate;
@@ -52,131 +65,131 @@ public class CampInformation implements Serializable {
         this.openTo = openTo;
 
         setDates(startDate, endDate);
-	}
-    
-	String getName() {
-		return this.name;
-	}
+    }
 
-	/**
-	 * 
-	 * @param name
-	 */
-	void setName(String name) {
-		this.name = name;
-	}
+    String getName() {
+        return this.name;
+    }
 
-	String getLocation() {
-		return this.location;
-	}
+    /**
+     * 
+     * @param name
+     */
+    void setName(String name) {
+        this.name = name;
+    }
 
-	/**
-	 * 
-	 * @param location
-	 */
-	void setLocation(String location) {
-		this.location = location;
-	}
+    String getLocation() {
+        return this.location;
+    }
 
-	/**
-	 * Get the Camp's event dates (not registration date).
+    /**
+     * 
+     * @param location
+     */
+    void setLocation(String location) {
+        this.location = location;
+    }
+
+    /**
+     * Get the Camp's event dates (not registration date).
      *
-	 * @return Camp's event dates in sorted order
-	 */
-	DateRange getCampDates() {
-		return this.campDates;
-	}
+     * @return Camp's event dates in sorted order
+     */
+    DateRange getCampDates() {
+        return this.campDates;
+    }
 
-	/**
-	 * Set the Camp's event dates (not registration date).
+    /**
+     * Set the Camp's event dates (not registration date).
      *
-	 * @param startDate The camp's first day
+     * @param startDate The camp's first day
      * @param endDate The camp's last day
-	 */
-	void setDates(Date startDate, Date endDate) {
+     */
+    void setDates(Date startDate, Date endDate) {
         assert !(startDate.isAfter(endDate));
         this.campDates = new DateRange(startDate, endDate);
-	}
+    }
 
-	Date getClosingDate() {
-		return this.closingDate;
-	}
+    Date getClosingDate() {
+        return this.closingDate;
+    }
 
-	/**
-	 * 
-	 * @param closingDate
-	 */
-	void setClosingDate(Date closingDate) {
-		this.closingDate = closingDate;
-	}
+    /**
+     * 
+     * @param closingDate
+     */
+    void setClosingDate(Date closingDate) {
+        this.closingDate = closingDate;
+    }
 
-	int getParticipantSlots() {
-		return this.participantSlots;
-	}
+    int getParticipantSlots() {
+        return this.participantSlots;
+    }
 
-	/**
-	 * 
-	 * @param participantSlots
-	 */
-	void setParticipantSlots(int participantSlots) {
-		this.participantSlots = participantSlots;
-	}
+    /**
+     * 
+     * @param participantSlots
+     */
+    void setParticipantSlots(int participantSlots) {
+        this.participantSlots = participantSlots;
+    }
 
-	int getCampCommSlots() {
-		return this.campCommSlots;
-	}
+    int getCampCommSlots() {
+        return this.campCommSlots;
+    }
 
-	/**
-	 * 
-	 * @param campCommSlots
-	 */
-	void setCampCommSlots(int campCommSlots) {
-		this.campCommSlots = campCommSlots;
-	}
+    /**
+     * 
+     * @param campCommSlots
+     */
+    void setCampCommSlots(int campCommSlots) {
+        this.campCommSlots = campCommSlots;
+    }
 
-	String getDescription() {
-		return this.description;
-	}
+    String getDescription() {
+        return this.description;
+    }
 
-	/**
-	 * 
-	 * @param description
-	 */
-	void setDescription(String description) {
-		this.description = description;
-	}
+    /**
+     * 
+     * @param description
+     */
+    void setDescription(String description) {
+        this.description = description;
+    }
 
-	Staff getStaffInCharge() {
-		return this.staffInCharge;
-	}
+    Staff getStaffInCharge() {
+        return this.staffInCharge;
+    }
 
-	/**
-	 * 
-	 * @param staffInCharge
-	 */
-	void setStaffInCharge(Staff staffInCharge) {
-		this.staffInCharge = staffInCharge;
-	}
+    /**
+     * 
+     * @param staffInCharge
+     */
+    void setStaffInCharge(Staff staffInCharge) {
+        this.staffInCharge = staffInCharge;
+    }
 
-	Collection<Faculty> getOpenTo() {
-		return this.openTo;
-	}
+    Collection<Faculty> getOpenTo() {
+        return this.openTo;
+    }
 
-	/**
-	 * 
-	 * @param openTo
-	 */
-	void setOpenTo(Collection<Faculty> faculties) {
-		this.openTo = faculties;
-	}
+    /**
+     * 
+     * @param openTo
+     */
+    void setOpenTo(Collection<Faculty> faculties) {
+        this.openTo = faculties;
+    }
 
-	String getCampID() {
+    String getCampID() {
         return campID;
-	}
+    }
 
-	void setCampID(String campID) {
+    void setCampID(String campID) {
         this.campID = campID;
-	}
+    }
 
 
 }
