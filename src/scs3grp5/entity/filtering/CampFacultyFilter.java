bb90f8b488f1f2d3d551638e7e21edf62bfc2fc4
ye-chuan <sengyechuan@gmail.com>
2023-11-27 03:17:56 +0800

    Added additional filter for CampFaculty

--- src/scs3grp5/entity/filtering/CampFacultyFilter.java
+++ src/scs3grp5/entity/filtering/CampFacultyFilter.java
@@ -14,10 +14,13 @@ public class CampFacultyFilter extends CampFilter {
 
     /** The only camps opened to exactly these faculties will be included */
 	private Set<Faculty> exactFaculties = new HashSet<Faculty>();
+    /** Only allow exact matches of the faculties to pass (not those subset etc) */
+    private Faculty asLongAsOpenedTo = null;
 
     /**
-     * Only include Camps that is open to this {@code Faculty} (not the whole school)
+     * Only include Camps that is open to only this {@code Faculty} (not the whole school)
      *  @param faculty The only faculty to allow through this filter
+     *  @return The {@code CampFacultyFilter} with this filter option
      */
     public static CampFacultyFilter onlyOpenedTo(Faculty faculty) {
         CampFacultyFilter filter = new CampFacultyFilter();
@@ -27,6 +30,7 @@ public class CampFacultyFilter extends CampFilter {
 
     /**
      * Only include Camps that is open to all faculties
+     *  @return The {@code CampFacultyFilter} with this filter option
      */
     public static CampFacultyFilter openedToAll() {
         CampFacultyFilter filter = new CampFacultyFilter();
@@ -34,10 +38,27 @@ public class CampFacultyFilter extends CampFilter {
         return filter;
     }
 
+    /**
+     * Only include Camps that is open to this {@code Faculty} (including those opened to whole school etc.)
+     *  @param faculty The faculty to allow through this filter
+     *  @return The {@code CampFacultyFilter} with this filter option
+     */
+    public static CampFacultyFilter openedTo(Faculty faculty) {
+        assert faculty != null;
+        CampFacultyFilter filter = new CampFacultyFilter();
+        filter.asLongAsOpenedTo = faculty;
+        return filter;
+    }
+
+
     /** {@inheritDoc} */
     @Override
     public boolean pass(Camp camp) {
         // Faculty Filtering
+        if (asLongAsOpenedTo != null) {
+            return camp.getOpenTo().contains(asLongAsOpenedTo);
+        }
+
         Set<Faculty> facultiesInCamp = new HashSet<Faculty>(camp.getOpenTo());
         if (!facultiesInCamp.equals(exactFaculties))
             return false;
